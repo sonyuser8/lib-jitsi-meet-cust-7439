712c77c2d848743b6ef6719aed865522200108db
stanran <sonyuser8@yahoo.com.tw>
2022-07-08 17:37:46 +0800

    adjust constraint

--- modules/RTC/ScreenObtainer.js
+++ modules/RTC/ScreenObtainer.js
@@ -168,11 +168,17 @@ const ScreenObtainer = {
                                     chromeMediaSourceId: streamId,
                                     minFrameRate: desktopSharingFrameRate?.min ?? SS_DEFAULT_FRAME_RATE,
                                     maxFrameRate: desktopSharingFrameRate?.max ?? SS_DEFAULT_FRAME_RATE,
-                                    maxWidth: window.screen.width,
-                                    maxHeight: window.screen.height
+                                    maxWidth: window.screen.width  * window.devicePixelRatio,
+                                    minWidth: window.screen.width * window.devicePixelRatio,
+                                    maxHeight: window.screen.height * window.devicePixelRatio,
+                                    minHeight: window.screen.height * window.devicePixelRatio
                                 }
                             }
                         };
+                        console.log('window.screen.width:', window.screen.width);
+                        console.log('window.screen.height:', window.screen.height);
+                        console.log('window.devicePixelRatio:', window.devicePixelRatio);
+                        console.log('constraints:',constraints);
 
                         // We have to use the old API on Electron to get a desktop stream.
                         navigator.mediaDevices.getUserMedia(constraints)
@@ -217,7 +223,12 @@ const ScreenObtainer = {
         const { desktopSharingFrameRate } = this.options;
         const setScreenSharingResolutionConstraints = browser.isChromiumBased()
             && this.options?.testing?.setScreenSharingResolutionConstraints;
-        let video = {};
+        // let video = {};
+        let video = { 
+            frameRate: desktopSharingFrameRate, 
+            width: { ideal: 1920, max: 1920 }, 
+            height: { ideal: 1080, max: 1080}
+        };
 
         if (typeof desktopSharingFrameRate === 'object') {
             video.frameRate = desktopSharingFrameRate;
